e80fb5676bac8836de2b2aaa309c9c52fc57e4fd
Chris Auer <cja@buildabonfire.com>
2022-04-14 12:28:03 -0400

    Update to global

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -11,19 +11,23 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            using (SentrySdk.Init(o =>
-            {
-                // Tells which project in Sentry to send events to:
-                o.Dsn = "https://6650da350516457ba17840fb4cf8c641@o92137.ingest.sentry.io/6208998";
-                // When configuring for the first time, to see what the SDK is doing:
-                o.Debug = true;
-                // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
-                // We recommend adjusting this value in production.
-                o.TracesSampleRate = 1.0;
-            }))
-            {
-                throw new Exception("MVC5");
-            }
+            //using (SentrySdk.Init(o =>
+            //{
+            //    // Tells which project in Sentry to send events to:
+            //    o.Dsn = "https://6650da350516457ba17840fb4cf8c641@o92137.ingest.sentry.io/6208998";
+            //    // When configuring for the first time, to see what the SDK is doing:
+            //    o.Debug = true;
+            //    // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+            //    // We recommend adjusting this value in production.
+            //    o.TracesSampleRate = 1.0;
+            //}))
+            //{
+            //    throw new Exception("MVC5");
+            //}
+
+            //SentrySdk.CaptureException(new Exception("CaptureException"));
+
+            throw new Exception("CaptureException");
 
             return View();
         }
--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -5,17 +5,53 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Sentry;
+using Sentry.AspNet;
 
 namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private IDisposable _sentry;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize Sentry to capture AppDomain unhandled exceptions and more.
+            _sentry = SentrySdk.Init(o =>
+            {
+                o.Dsn = "https://6650da350516457ba17840fb4cf8c641@o92137.ingest.sentry.io/6208998";
+                // When configuring for the first time, to see what the SDK is doing:
+                o.Debug = true;
+                // Set TracesSampleRate to 1.0 to capture 100%
+                // of transactions for performance monitoring.
+                // We recommend adjusting this value in production
+                o.TracesSampleRate = 1.0;
+                // if you also installed the Sentry.EntityFramework package
+                o.AddAspNet();
+            });
+        }
+
+        protected void Application_Error() => Server.CaptureLastError();
+
+        protected void Application_BeginRequest()
+        {
+            Context.StartSentryTransaction();
+        }
+
+        protected void Application_EndRequest()
+        {
+            Context.FinishSentryTransaction();
+        }
+
+        protected void Application_End()
+        {
+            // Flushes out events before shutting down.
+            _sentry?.Dispose();
         }
     }
 }
