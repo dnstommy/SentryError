e35e8562681b02fdf779ebb906f9e35dcc4a5309
Chris Auer <cja@buildabonfire.com>
2022-04-14 08:06:50 -0400

    Added console and raven

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Sentry;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SentrySdk.Init(o =>
+            {
+                // Tells which project in Sentry to send events to:
+                o.Dsn = "https://6650da350516457ba17840fb4cf8c641@o92137.ingest.sentry.io/6208998";
+                // When configuring for the first time, to see what the SDK is doing:
+                o.Debug = true;
+                // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+                // We recommend adjusting this value in production.
+                o.TracesSampleRate = 1.0;
+            }))
+            {
+                throw new Exception("Console App");
+            }
+
+        }
+    }
+}
---
+++ Raven/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SharpRaven;
+using SharpRaven.Data;
+
+namespace Raven.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var ravenClient = new RavenClient("https://6650da350516457ba17840fb4cf8c641@o92137.ingest.sentry.io/6208998");
+            ravenClient.Capture(new SentryEvent(new Exception("Raven")));
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
